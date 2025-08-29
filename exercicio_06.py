4c86c417c491930f0dad22b08a3ef5e91155dea2
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-08-29 18:22:37 -0300

    update exercicio_06.py

---
+++ exercicio_06.py
@@ -0,0 +1,10 @@
+"""6. Somando Valores
+Crie uma lista com 3 números inteiros. Use um laço for para somar todos os números da lista
+e imprima o resultado."""
+
+numeros = [10, 15, 20]
+soma = 0
+for numero in numeros:
+    soma += numero
+print("A soma é:", soma) 
+  

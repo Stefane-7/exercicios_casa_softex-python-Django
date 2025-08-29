20922cc69de20e9fbafc79d80e2bfe30c8615928
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-08-29 04:38:23 -0300

    update exercicio_02.py

--- exercicio_02.py
+++ exercicio_02.py
@@ -1,3 +1,7 @@
+"""2. Adicionando com .append()
+Crie uma lista vazia. Peça ao usuário para digitar o nome de um filme e adicione-o à lista.
+Imprima a lista no final."""
+
 lista = []
 nome_filme = input("Digite o nome de um filme: ")
 lista.append(nome_filme)

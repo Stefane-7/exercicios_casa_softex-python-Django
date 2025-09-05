7f78c1ec4ed6710584964a1dc383763b63639f35
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-05 20:54:58 -0300

    create Modulo_02/Aula_02/exercicio_03.py

---
+++ Modulo_02/Aula_02/exercicio_03.py
@@ -0,0 +1,6 @@
+"""3. Contando Elementos: Crie uma tupla numeros = (1, 2, 3, 2, 4, 2). Use o método .count()
+para descobrir quantas vezes o número 2 aparece"""
+
+numeros = (1, 2, 3, 2, 4, 2)
+print(numeros.count(2))
+#o numero '2' aparece 3 vezes

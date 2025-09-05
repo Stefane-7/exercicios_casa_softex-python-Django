ee71558c03607dd1bd90999478d7ed8ad5d626af
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-05 20:56:31 -0300

    create Modulo_02/Aula_02/exercicio_06.py

---
+++ Modulo_02/Aula_02/exercicio_06.py
@@ -0,0 +1,8 @@
+"""6. Adicionar e Remover do Conjunto: Crie um conjunto linguas = {'português', 'inglês'}.
+Adicione 'espanhol' e depois remova 'português'. Imprima o conjunto final."""
+
+
+linguas = {'português', 'inglês'}
+linguas.add('espanhol')
+linguas.remove('português')
+print(linguas)

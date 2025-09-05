2944c851f42ca562dd8dde5ef13c28ab2b80f255
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-05 20:55:20 -0300

    create Modulo_02/Aula_02/exercicio_04.py

---
+++ Modulo_02/Aula_02/exercicio_04.py
@@ -0,0 +1,6 @@
+"""4. Encontrando um Elemento: Crie a tupla nomes = ('Carlos', 'Ana', 'Pedro'). Use o método
+.index() para encontrar o índice do nome 'Ana'."""
+
+nomes = ('Carlos', 'Ana', 'Pedro')
+print(nomes.index('Ana'))
+# Aparece no indice 1

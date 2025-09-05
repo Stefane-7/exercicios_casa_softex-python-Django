a71aa461646297f21737ae7bdf94571076d0a540
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-05 20:55:52 -0300

    create Modulo_02/Aula_02/exercicio_05.py

---
+++ Modulo_02/Aula_02/exercicio_05.py
@@ -0,0 +1,7 @@
+"""5. Criação de Conjunto e Unicidade: Crie um conjunto chamado produtos com os
+elementos 'pão', 'leite', 'pão', 'queijo'. Imprima o conjunto e observe a duplicação."""
+
+produtos = {'pão', 'leite', 'pão', 'queijo'}
+print(produtos)
+
+# O elemento 'pão' aparece uma unica vez

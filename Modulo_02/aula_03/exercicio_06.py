71d26c7a0b72f9d32f49aef2c743e44744f82511
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-05 21:55:33 -0300

    update Modulo_02/aula_03/exercicio_06.py

--- Modulo_02/aula_03/exercicio_06.py
+++
@@ -1,3 +0,0 @@
-"""5. Crie um dicionário com 3 produtos e suas quantidades em estoque. Peça ao usuário para
-digitar um nome de produto e uma quantidade para adicionar. Atualize o estoque e
-imprima o dicionário."""

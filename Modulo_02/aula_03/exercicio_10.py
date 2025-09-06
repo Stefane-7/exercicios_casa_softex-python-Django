15f6176399ef405b1f5b70fd0f49a9564282a015
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-05 22:05:06 -0300

    update Modulo_02/aula_03/exercicio_10.py

---
+++ Modulo_02/aula_03/exercicio_10.py
@@ -0,0 +1,3 @@
+"""10. Crie um dicionário com cores e seus códigos hexadecimais. Use um loop while para
+permitir que o usuário procure o código de uma cor repetidamente. O loop deve terminar
+quando o usuário digitar "sair"."""

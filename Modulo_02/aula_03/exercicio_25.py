d476e381f36f5114551a68c324ada7ed47343ceb
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-05 22:24:58 -0300

    update Modulo_02/aula_03/exercicio_25.py

---
+++ Modulo_02/aula_03/exercicio_25.py
@@ -0,0 +1,3 @@
+"""25. Gerenciamento de Tarefas: Crie um dicionário para gerenciar tarefas. As chaves são os
+nomes das tarefas, e os valores são listas de status (ex: ["pendente", "em andamento",
+"concluído"]). Peça ao usuário para adicionar uma tarefa e seu status. Depois, imprima"""

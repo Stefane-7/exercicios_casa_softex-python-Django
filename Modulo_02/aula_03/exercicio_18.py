8d3bbcd577fb6dcc83ba3b2d3985ee8e371e5789
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-05 22:15:37 -0300

    update Modulo_02/aula_03/exercicio_18.py

---
+++ Modulo_02/aula_03/exercicio_18.py
@@ -0,0 +1,2 @@
+"""18. União de Dicionários: Crie dois dicionários. Crie um terceiro dicionário que seja a união
+dos dois primeiros. Se uma chave existir em ambos, use o valor do segundo dicionário."""

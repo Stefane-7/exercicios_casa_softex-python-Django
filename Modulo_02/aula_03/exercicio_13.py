92408ca94910d3d4b4d2a651ad152a7e3572ef1a
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-06 15:43:56 -0300

    update Modulo_02/aula_03/exercicio_13.py

--- Modulo_02/aula_03/exercicio_13.py
+++ Modulo_02/aula_03/exercicio_13.py
@@ -2,22 +2,6 @@
 e os valores, o nome de um local. Peça ao usuário para digitar as coordenadas e imprima
 o local."""
 
-"""mapa = {
-    (0, 0): "Praça Central",
-    (1, 2): "Supermercado",
-    (3, 4): "Escola",
-    (5, 1): "Hospital"
-}
-
-entrada = input("Digite uma coordenada: ")
-for coordenada, local in mapa.items():
-  if entrada == coordenada:
-    print(f"O local correspondente é : {local}")
-    break
-  else:
-    print(f"Não foi encontrada nenhuma correspondecia para: '{entrada}'")
-    break"""
-    
 mapa = {
     (0, 0): "Praça Central",
     (1, 2): "Supermercado",

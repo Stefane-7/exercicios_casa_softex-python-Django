1d119da92e0d50d740977e02f79ed19793524bbf
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-05 22:22:28 -0300

    update Modulo_02/aula_03/exercicio_21.py

---
+++ Modulo_02/aula_03/exercicio_21.py
@@ -0,0 +1,3 @@
+"""21. Agenda de Contatos: Crie um sistema simples de agenda que use um dicionário. Use
+um loop while para mostrar um menu com opções de "adicionar contato", "buscar
+contato" e "sair"."""

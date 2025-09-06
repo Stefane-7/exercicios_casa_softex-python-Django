8cab86e44655298df6f4aaf2d2f48dd4baad8c0c
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-05 21:52:26 -0300

    update Modulo_02/aula_03/exercicio_04.py

---
+++ Modulo_02/aula_03/exercicio_04.py
@@ -0,0 +1,3 @@
+"""4. Crie um dicionário com os nomes de 3 países e suas capitais. Peça ao usuário para
+digitar o nome de um país e imprima a capital correspondente. Use dicionario.get() para
+evitar erros."""

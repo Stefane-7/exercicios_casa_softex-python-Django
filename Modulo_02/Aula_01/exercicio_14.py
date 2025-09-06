44eb82597021af4de44d6fca5ac2f584fe88a212
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-05 22:41:28 -0300

    update Modulo_02/Aula_01/exercicio_14.py

---
+++ Modulo_02/Aula_01/exercicio_14.py
@@ -0,0 +1,3 @@
+"""14. Usando abs()
+Crie uma lista com os números [-5, 8, -2, 10]. Use um laço for para criar uma nova lista
+contendo o valor absoluto (positivo) de cada número. Imprima a nova lista."""

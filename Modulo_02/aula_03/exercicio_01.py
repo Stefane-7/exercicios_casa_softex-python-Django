a7f97fcde9a0d1504ed6de29e213048aa5a85edc
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-06 11:29:15 -0300

    update Modulo_02/aula_03/exercicio_01.py

--- Modulo_02/aula_03/exercicio_01.py
+++ Modulo_02/aula_03/exercicio_01.py
@@ -1,1 +1,5 @@
 """1. Crie um dicionário com 3 nomes de frutas e seus preços. Imprima o dicionário completo."""
+
+frutas = {'banana': 15.99, 'maçã': 11.00, 'pera': 16.80}
+print(frutas)
+print(frutas['pera'])

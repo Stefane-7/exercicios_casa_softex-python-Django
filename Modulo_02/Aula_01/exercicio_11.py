a383b9cda0c2a2d479427735f4f268d3764fa511
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-05 22:37:58 -0300

    update Modulo_02/Aula_01/exercicio_11.py

--- Modulo_02/Aula_01/exercicio_11.py
+++ Modulo_02/Aula_01/exercicio_11.py
@@ -2,5 +2,7 @@
 Use range() para criar uma lista com os números de 1 a 10. Percorra a lista com um laço for e
 imprima cada número"""
 
-for num in range(1,11):
+numeros = list(range(1,11))
+
+for num in numeros:
     print(num)

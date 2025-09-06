aafa3f059772fef8f0868a9b53fa045cacb870f8
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-05 21:21:02 -0300

    create Modulo_02/Aula_01/exercicio_15.py

---
+++ Modulo_02/Aula_01/exercicio_15.py
@@ -0,0 +1,8 @@
+"""15. Simples pop()
+Crie uma lista com 5 tarefas. Use o método .pop() para remover a última tarefa da lista.
+Imprima a tarefa que foi removida e a lista atualizada"""
+
+tarefas = ["fazer compras", "academia", "trabalho", "limpar casa", "fazer unhas"]
+removido = tarefas.pop(1)
+print(removido)
+print(tarefas)

3f510af7067e5b87cc6329cc996b55c5f6178fbb
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-06 15:20:42 -0300

    update Modulo_02/aula_03/exercicio_12.py

--- Modulo_02/aula_03/exercicio_12.py
+++ Modulo_02/aula_03/exercicio_12.py
@@ -1,1 +1,8 @@
 """12. Crie um dicionário onde a chave é o nome de um time e o valor é uma lista com os nomes de 3 jogadores. Imprima o nome do segundo jogador do segundo time."""
+
+times = {
+    "Flamengo": ["Gabigol", "Arrascaeta", "Everton Ribeiro"],
+    "Palmeiras": ["Dudu", "Rony", "Weverton"],
+    "Corinthians": ["Róger Guedes", "Fábio Santos", "Cássio"]
+}
+

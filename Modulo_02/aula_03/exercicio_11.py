c96eb4bbad94f789e66e080cf10c9748878e945b
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-05 22:00:34 -0300

    update Modulo_02/aula_03/exercicio_11.py

--- Modulo_02/aula_03/exercicio_11.py
+++
@@ -1,3 +0,0 @@
-"""8. Crie um dicionário com produtos e preços. Peça ao usuário para digitar um produto para
-remover. Remova-o do dicionário e imprima o dicionário final. Use if para verificar se o
-produto existe antes de remover."""

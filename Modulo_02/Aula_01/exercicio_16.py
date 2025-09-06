b4a09cc5e8b6395866aa48944b9e3f5259402c11
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-05 21:21:30 -0300

    create Modulo_02/Aula_01/exercicio_16.py

---
+++ Modulo_02/Aula_01/exercicio_16.py
@@ -0,0 +1,4 @@
+"""16. Fila com pop() e append()
+Crie uma lista para ser uma fila de atendimento (['cliente1', 'cliente2', 'cliente3']). Use append()
+para adicionar um novo cliente e pop(0) para "atender" o primeiro cliente da fila. Imprima a
+lista após cada operação."""

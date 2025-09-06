e842ff40f4b867f6f1f0c91e756dce9c7916234a
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-06 15:06:22 -0300

    update Modulo_02/aula_03/exercicio_11.py

--- Modulo_02/aula_03/exercicio_11.py
+++ Modulo_02/aula_03/exercicio_11.py
@@ -1,2 +1,12 @@
 """11. Crie uma lista de dicionários, onde cada dicionário representa um filme com "título" e
 "ano". Imprima o ano do segundo filme da lista."""
+
+filmes = [
+    {"título": "O Poderoso Chefão", "ano": 1972},
+    {"título": "Forrest Gump", "ano": 1994},
+    {"título": "A Origem", "ano": 2010},
+    {"título": "Matrix", "ano": 1999},
+    {"título": "O Senhor dos Anéis: A Sociedade do Anel", "ano": 2001}
+]
+
+print(filmes[1]["ano"])  

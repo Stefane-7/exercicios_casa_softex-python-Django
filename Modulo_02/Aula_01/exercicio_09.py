bdc2de0d94b54ddd69bec5090c4f4ea60f4d4d71
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-05 22:32:57 -0300

    update Modulo_02/Aula_01/exercicio_09.py

--- Modulo_02/Aula_01/exercicio_09.py
+++ Modulo_02/Aula_01/exercicio_09.py
@@ -1,6 +1,7 @@
 """9. sort() e reverse()
 Crie uma lista com 5 nomes de cidades (não em ordem alfabética). Use o método .sort() para
 ordenar a lista. Depois, use .reverse() para inverter a ordem. Imprima a lista nos dois"""
+
 cidades = ["maricá", "são gonçalo", "lapa", "niteroi","bom-sucesso"]
 cidades.sort()
 print(cidades)

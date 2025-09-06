c4a59e1b44022d597a783a1d1f29e9631c6c21e4
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-05 22:24:30 -0300

    update Modulo_02/aula_03/exercicio_24.py

---
+++ Modulo_02/aula_03/exercicio_24.py
@@ -0,0 +1,3 @@
+"""24. Sistema de Inventário: Crie um dicionário de inventário de um jogo (ex: "espada":
+{"dano": 50, "peso": 2.5}). Permita que o usuário insira um item no inventário com suas
+características e liste todos os itens."""

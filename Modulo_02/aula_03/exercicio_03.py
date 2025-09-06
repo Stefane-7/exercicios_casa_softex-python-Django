a6917590c4c4b4242b01e7af055a3083f860cd93
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-06 11:43:11 -0300

    update Modulo_02/aula_03/exercicio_03.py

--- Modulo_02/aula_03/exercicio_03.py
+++ Modulo_02/aula_03/exercicio_03.py
@@ -1,1 +1,7 @@
 """3. Crie um dicionário vazio. Adicione 3 pares de chave-valor. Imprima o dicionário."""
+
+dicionario = {}
+dicionario['sei-lá'] = 'alguma coisa'
+dicionario['algo'] = 'não sei'
+dicionario['batata'] = 'é melhor frita'
+print(dicionario)

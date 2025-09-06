d6a09e9541056185fb2135e9add846cafefccaf1
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-06 12:38:55 -0300

    update Modulo_02/aula_03/exercicio_06.py

--- Modulo_02/aula_03/exercicio_06.py
+++ Modulo_02/aula_03/exercicio_06.py
@@ -2,3 +2,5 @@
 idade no formato "Nome: [Nome], Idade: [Idade]"""
 
 usuarios = {'Maria': 21, 'Pedro': 30, 'Carlos': 25, 'Julia': 34}
+for nome, idade in usuarios.items():
+  print(f"Nome: {nome}, Idade: {idade}")

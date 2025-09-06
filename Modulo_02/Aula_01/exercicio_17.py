4369ea882e7c20fbd1721712ebe4eeaf28b0b7f6
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-05 22:43:12 -0300

    update Modulo_02/Aula_01/exercicio_17.py

---
+++ Modulo_02/Aula_01/exercicio_17.py
@@ -0,0 +1,4 @@
+"""17. Fila com while
+Crie uma fila de atendimento com 3 nomes. Use um laço while para "atender" um cliente de
+cada vez (pop(0)) e imprima quem está sendo atendido a cada passo. O laço deve continuar
+enquanto houver clientes na fila."""

46797b3c9e20fedc279b680d4ac1e0066d662e97
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-05 22:57:35 -0300

    update Modulo_02/Aula_01/exercicio_17.py

--- Modulo_02/Aula_01/exercicio_17.py
+++ Modulo_02/Aula_01/exercicio_17.py
@@ -2,3 +2,9 @@
 Crie uma fila de atendimento com 3 nomes. Use um laço while para "atender" um cliente de
 cada vez (pop(0)) e imprima quem está sendo atendido a cada passo. O laço deve continuar
 enquanto houver clientes na fila."""
+
+pacientes = ['Maria', 'Pedro', 'Carla']
+
+while pacientes:
+  em_atendimento = pacientes.pop(0)
+  print(f"Paciente em atendimento: {em_atendimento}")

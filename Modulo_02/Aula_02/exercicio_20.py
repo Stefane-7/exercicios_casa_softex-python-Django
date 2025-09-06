ddb3af3f0ed057f401cf68ac635ca1371c240303
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-05 21:04:52 -0300

    create Modulo_02/Aula_02/exercicio_20.py

---
+++ Modulo_02/Aula_02/exercicio_20.py
@@ -0,0 +1,4 @@
+"""20. Registro de Acessos Únicos: Você tem uma lista de tuplas (usuario_id, page_id)
+representando acessos de usuários a páginas. Crie um conjunto de tuplas para
+armazenar todos os acessos únicos, removendo duplicações.
+acessos = [(1, 'home'), (2, 'produtos'), (1, 'home'), (3, 'contato')]"""

21b957c7d34a8fa7dc057a9a82fcdccaf4e434c0
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-06 15:27:29 -0300

    update Modulo_02/aula_03/exercicio_13.py

--- Modulo_02/aula_03/exercicio_13.py
+++ Modulo_02/aula_03/exercicio_13.py
@@ -1,3 +1,12 @@
 """13. Crie um dicionário que use tuplas como chaves. As chaves devem ser coordenadas (x, y)
 e os valores, o nome de um local. Peça ao usuário para digitar as coordenadas e imprima
 o local."""
+
+mapa = {
+    (0, 0): "Praça Central",
+    (1, 2): "Supermercado",
+    (3, 4): "Escola",
+    (5, 1): "Hospital"
+}
+
+

7d7ff5a9e3906d337a432b2289f00e6797ce7f25
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-05 22:16:56 -0300

    update Modulo_02/aula_03/exercicio_20.py

---
+++ Modulo_02/aula_03/exercicio_20.py
@@ -0,0 +1,3 @@
+"""20. Vendas Simples: Crie um dicionário de estoque e outro de vendas. Simule uma venda,
+diminuindo a quantidade no estoque e adicionando a venda ao dicionário de vendas. Use
+if para garantir que há estoque suficiente."""

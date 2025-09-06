f5bc58f5ca8273d066d86367b937c5d77749cf3c
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-06 13:42:02 -0300

    update Modulo_02/aula_03/exercicio_10.py

--- Modulo_02/aula_03/exercicio_10.py
+++ Modulo_02/aula_03/exercicio_10.py
@@ -1,3 +1,36 @@
 """10. Crie um dicionário com cores e seus códigos hexadecimais. Use um loop while para
 permitir que o usuário procure o código de uma cor repetidamente. O loop deve terminar
 quando o usuário digitar "sair"."""
+
+cores_hex = {
+    "preto": "#000000",
+    "branco": "#FFFFFF",
+    "vermelho": "#FF0000",
+    "verde": "#00FF00",
+    "azul": "#0000FF",
+    "amarelo": "#FFFF00",
+    "ciano": "#00FFFF",
+    "magenta": "#FF00FF",
+    "laranja": "#FFA500",
+    "rosa": "#FFC0CB",
+    "roxo": "#800080",
+    "marrom": "#A52A2A",
+    "cinza": "#808080",
+    "turquesa": "#40E0D0",
+    "dourado": "#FFD700"
+}
+
+while True:
+  try:
+    entrada = input("Digite o código correspondente a cor que deseja(ou 'sair' para parar): ").lower()
+    if entrada == 'sair':
+      print("Encerrando...")
+      break
+    for cor, codigo in entrada.items():
+      if codigo in entrada:
+        print(f"A cor correspondente ao codigo digitado é: {cor}")
+      else:
+        print(f"Nenhuma correspondencia encontrada para o código digitado: '{entrada}' ")
+  except ValueError:
+    print("Por favor digite dados válidos.")
+      

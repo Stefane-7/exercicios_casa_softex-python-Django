18db4eea30a26560a304b731410362ab0ea8b183
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-06 12:13:40 -0300

    update Modulo_02/aula_03/exercicio_05.py

--- Modulo_02/aula_03/exercicio_05.py
+++ Modulo_02/aula_03/exercicio_05.py
@@ -1,3 +1,16 @@
 """5. Crie um dicionário com 3 produtos e suas quantidades em estoque. Peça ao usuário para
 digitar um nome de produto e uma quantidade para adicionar. Atualize o estoque e
 imprima o dicionário."""
+
+"""5. Crie um dicionário com 3 produtos e suas quantidades em estoque. Peça ao usuário para
+digitar um nome de produto e uma quantidade para adicionar. Atualize o estoque e
+imprima o dicionário."""
+
+estoque = {'cobertores': 10, 'lençois': 15, 'toalhas': 7}
+print(estoque)
+produto = input("Digite o produto a ser adicionado no estoque: ")
+quantidade = int(input("Digite a quantidade: "))
+
+estoque[produto] = quantidade
+
+print(f"Estoque após atualização: {estoque}")

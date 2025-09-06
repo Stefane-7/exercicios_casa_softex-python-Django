f16648258cc5aaca4616cc05010ea666eb25ea22
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-05 21:15:34 -0300

    create Modulo_02/Aula_01/exercicio_04.py

---
+++ Modulo_02/Aula_01/exercicio_04.py
@@ -0,0 +1,5 @@
+""""4. Tamanho da Lista
+Crie uma lista com 5 frutas. Use len() para descobrir e imprimir quantos itens a lista tem."""
+
+lista = ['macã', 'banana', 'pessego', 'uva', 'maracujá']
+print(len(lista))

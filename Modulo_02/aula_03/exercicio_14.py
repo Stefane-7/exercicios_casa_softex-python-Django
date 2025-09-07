7ea26060dab5b4dc9751b238b8ca472aceef5333
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-07 20:06:13 -0300

    update Modulo_02/aula_03/exercicio_14.py

--- Modulo_02/aula_03/exercicio_14.py
+++ Modulo_02/aula_03/exercicio_14.py
@@ -1,1 +1,14 @@
 """14. Dada uma lista de palavras, crie um dicionário que conte a frequência de cada palavra."""
+
+
+palavras = ["gato", "cachorro", "gato", "pássaro", "gato", "cachorro"]
+
+frequencia = {}  
+
+for palavra in palavras:
+    if palavra in frequencia:
+        frequencia[palavra] += 1  
+    else:
+        frequencia[palavra] = 1
+
+print(frequencia)

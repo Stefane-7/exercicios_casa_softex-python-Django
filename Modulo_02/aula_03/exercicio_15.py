7b4ea5ea45a7d1c0a68f889494ec3e820b4e0764
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-07 20:31:37 -0300

    update Modulo_02/aula_03/exercicio_15.py

--- Modulo_02/aula_03/exercicio_15.py
+++ Modulo_02/aula_03/exercicio_15.py
@@ -1,12 +1,12 @@
 """15. Crie um dicionário com os nomes de 3 pessoas e seus hobbies (em formato de conjunto).
 Peça ao usuário para digitar o nome de uma pessoa e imprima seus hobbies."""
 
-# Criando o dicionário com nomes e hobbies
 pessoas = {
     "Maria": {"ler", "cantar", "nadar"},
     "João": {"jogar futebol", "cozinhar", "viajar"},
     "Ana": {"desenhar", "assistir filmes", "dançar"}
 }
+
 
 nome = input("Digite o nome da pessoa: ").capitalize()
 

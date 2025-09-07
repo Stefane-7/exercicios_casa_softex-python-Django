9ea4851010224e1809247f6059b8336a305f976d
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-07 20:34:40 -0300

    update Modulo_02/aula_03/exercicio_17.py

--- Modulo_02/aula_03/exercicio_17.py
+++ Modulo_02/aula_03/exercicio_17.py
@@ -1,2 +1,14 @@
 """17. Criação com Condição: Crie um dicionário a partir de uma lista de números, onde a
 chave é o número e o valor é "par" ou "ímpar"."""
+
+numeros = [1, 2, 3, 4, 5, 6]
+
+paridade = {}  
+
+for n in numeros:
+    if n % 2 == 0:
+        paridade[n] = "par"
+    else:
+        paridade[n] = "ímpar"
+
+print(paridade)

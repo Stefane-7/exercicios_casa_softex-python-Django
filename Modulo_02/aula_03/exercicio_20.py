335659b0142ff3d7571179f418688698429ba466
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-07 20:49:57 -0300

    update Modulo_02/aula_03/exercicio_20.py

--- Modulo_02/aula_03/exercicio_20.py
+++ Modulo_02/aula_03/exercicio_20.py
@@ -1,3 +1,35 @@
 """20. Vendas Simples: Crie um dicionário de estoque e outro de vendas. Simule uma venda,
 diminuindo a quantidade no estoque e adicionando a venda ao dicionário de vendas. Use
 if para garantir que há estoque suficiente."""
+
+
+estoque = {
+    "camiseta": 10,
+    "calça": 5,
+    "sapato": 3
+}
+
+
+vendas = {}
+
+produto = input("Digite o nome do produto: ").lower()
+quantidade = int(input("Digite a quantidade que deseja comprar: "))
+
+
+if produto in estoque:
+    if estoque[produto] >= quantidade:
+        estoque[produto] -= quantidade  
+        
+        if produto in vendas:
+            vendas[produto] += quantidade
+        else:
+            vendas[produto] = quantidade
+        
+        print(f"Venda realizada! Estoque restante de {produto}: {estoque[produto]}")
+    else:
+        print("Quantidade indisponível no estoque.")
+else:
+    print("Produto não encontrado.")
+
+print("\nEstoque final:", estoque)
+print("Vendas realizadas:", vendas)

9d43b5f2c0130475fcc5bec722f304022ab6d8c0
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-07 21:21:35 -0300

    update Modulo_02/Aula_02/exercicio_20.py

--- Modulo_02/Aula_02/exercicio_20.py
+++ Modulo_02/Aula_02/exercicio_20.py
@@ -2,3 +2,10 @@
 representando acessos de usuários a páginas. Crie um conjunto de tuplas para
 armazenar todos os acessos únicos, removendo duplicações.
 acessos = [(1, 'home'), (2, 'produtos'), (1, 'home'), (3, 'contato')]"""
+
+
+acessos = [(1, 'home'), (2, 'produtos'), (1, 'home'), (3, 'contato')]
+
+acessos_unicos = set(acessos)
+
+print(acessos_unicos)

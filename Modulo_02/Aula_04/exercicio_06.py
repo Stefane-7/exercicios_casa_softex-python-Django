a1ee3522b07a2fd98a5d8d2de0483a90236cc4ab
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-09 19:28:27 -0300

    update Modulo_02/Aula_04/exercicio_06.py

--- Modulo_02/Aula_04/exercicio_06.py
+++ Modulo_02/Aula_04/exercicio_06.py
@@ -1,1 +1,8 @@
 """6. Cálculo de Média: Crie uma função que receba três notas e retorne a média delas."""
+
+def verificador_de_media(n1: int, n2: int, n3: int) -> float:
+  media = (n1 + n2 + n3) / 3
+  return media
+resultado = verificador_de_media(26, 28, 53)
+print(f"A média das idades é: {resultado:.2f}")
+  

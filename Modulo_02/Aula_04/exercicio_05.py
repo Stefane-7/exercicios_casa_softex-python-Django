4a1a42a6f35394aa483019ae20f0bd43e7f2e1af
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-09 19:35:13 -0300

    update Modulo_02/Aula_04/exercicio_05.py

--- Modulo_02/Aula_04/exercicio_05.py
+++ Modulo_02/Aula_04/exercicio_05.py
@@ -1,2 +1,6 @@
 """5. Área de um Retângulo: Crie uma função que receba a base e a altura de um retângulo e
 retorne sua área."""
+
+def retangulo_area(base: float, altura: float) -> float:
+  return base * altura
+print(f"A área do retangulo é: {retangulo_area(20, 25)}")

598842844849a1c87d46d1fb3f630f18b32cadca
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-09 18:36:40 -0300

    update Modulo_02/Aula_04/exercicio_19.py

---
+++ Modulo_02/Aula_04/exercicio_19.py
@@ -0,0 +1,3 @@
+"""19. Ordenador de Dicionários com lambda: Crie uma função que receba uma lista de
+dicionários (cada um com "nome" e "idade"). Use uma lambda e um loop para ordenar a
+lista pela idade e retornar a lista ordenada."""

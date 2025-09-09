bf07aa9b86fbe9efc17122604188329aa0239a94
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-09 18:37:29 -0300

    update Modulo_02/Aula_04/exercicio_21.py

---
+++ Modulo_02/Aula_04/exercicio_21.py
@@ -0,0 +1,3 @@
+"""21. Validador de Entrada Numérica: Crie uma função que solicite ao usuário que digite um
+número inteiro. Use um loop e try/except para garantir que a entrada seja válida. A
+função deve retornar o número inteiro."""

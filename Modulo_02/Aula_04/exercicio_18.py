6ed4f6287d1d271819611f9165096fb20820a247
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-09 18:35:48 -0300

    update Modulo_02/Aula_04/exercicio_18.py

---
+++ Modulo_02/Aula_04/exercicio_18.py
@@ -0,0 +1,3 @@
+"""18. Filtro de Lista com lambda: Crie uma função que receba uma lista de números e use
+um loop para retornar uma nova lista apenas com os números pares. Use uma lambda
+dentro do loop"""

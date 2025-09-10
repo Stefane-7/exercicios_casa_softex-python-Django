572cc193e469a25c9e5eca3c15e6f3d3103244be
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-09 22:33:44 -0300

    update Modulo_02/Aula_04/exercicio_23.py

--- Modulo_02/Aula_04/exercicio_23.py
+++ Modulo_02/Aula_04/exercicio_23.py
@@ -1,2 +1,25 @@
 """23. Autenticador de Usuário: Crie uma função autenticar(usuario, senha) que verifica se o
 usuário e a senha existem em um dicionário pré-definido. """
+
+
+usuarios = {
+    "joao": "1234",
+    "maria": "abcd",
+    "pedro": "senha123"
+}
+
+def autenticar(usuario: str, senha: str) -> bool:
+    if usuario in usuarios:
+        return usuarios[usuario] == senha
+    return False
+
+
+while True:
+    usuario_entrada = input("Digite seu usuário: ").lower()
+    senha_entrada = input("Digite sua senha: ")
+    
+    if autenticar(usuario_entrada, senha_entrada):
+        print("Login realizado com sucesso!")
+        break  
+    else:
+        print("Usuário ou senha incorretos. Tente novamente.")

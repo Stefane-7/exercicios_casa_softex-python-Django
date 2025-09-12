c3cafc11dda1629da05734a150871a68e3e0deea
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-12 17:22:17 -0300

    update Modulo_02/aula_04_/exercicio_09.py

--- Modulo_02/aula_04_/exercicio_09.py
+++ Modulo_02/aula_04_/exercicio_09.py
@@ -1,2 +1,17 @@
 """9. Encontrar o Maior Número: Crie uma função que receba uma lista de números e
 retorne o maior número da lista. Use um loop. Se a lista estiver vazia, retorne None."""
+
+def maior_numero(lista: list) -> int | None:
+    if not lista:   
+        return None
+    
+    maior = lista[0]  
+    for numero in lista:
+        if numero > maior:
+            maior = numero
+    return maior
+
+
+
+print(maior_numero([5, 9, 2, 17, 3]))  
+print(maior_numero([]))  

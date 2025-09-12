a36445aeda9c7e3582e9991be29a8bf1bb1437d6
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-12 18:55:07 -0300

    update Modulo_02/aula_04_/exercicio_11.py

--- Modulo_02/aula_04_/exercicio_11.py
+++ Modulo_02/aula_04_/exercicio_11.py
@@ -1,21 +1,22 @@
 """11. Calculadora de IMC: Crie uma função que receba o peso (em kg) e a altura (em metros)
 e retorne o Índice de Massa Corporal (IMC). A fórmula é: IMC = peso / (altura ** 2)."""
 
-def calcular_imc(peso: float, altura: float) -> float | None:
-  imc = peso / (altura ** 2)
-  return imc
+def calcular_imc(peso: float, altura: float) -> float:
+    imc = peso / (altura ** 2)
+    return imc
   
 def entrada_dados():
-  try:
-    peso = float(input("Digite seu peso: "))#.replace(",", ".")
-    altura = float(input("Digite sua altura: "))#.replace(",", ".")
-  except ValueError:
-    print("Digite apenas números.")
-    return peso, altura
-def executando():
-  peso, altura = entrada_dados()
-  imc = calcular_imc(peso, altura)
+    while True:
+        try:
+            peso = float(input("Digite seu peso (kg): ").replace(",", "."))
+            altura = float(input("Digite sua altura (m): ").replace(",", "."))
+            return peso, altura
+        except ValueError:
+            print("Digite apenas números válidos.")
   
-  print(f"O IMC referente aos dados digitados é {imc}")
+def executando():
+    peso, altura = entrada_dados()
+    imc = calcular_imc(peso, altura)
+    print(f"O IMC referente aos dados digitados é {imc:.2f}")
 
 executando()

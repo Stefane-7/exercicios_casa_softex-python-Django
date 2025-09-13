a431b22d54202645c4f200e0c113ad5738762ad2
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-13 14:20:41 -0300

    update Modulo_02/aula_04_/exercicio_18.py

--- Modulo_02/aula_04_/exercicio_18.py
+++ Modulo_02/aula_04_/exercicio_18.py
@@ -1,3 +1,15 @@
 """18. Filtro de Lista com lambda: Crie uma função que receba uma lista de números e use
 um loop para retornar uma nova lista apenas com os números pares. Use uma lambda
 dentro do loop"""
+
+def filtrar_pares(numeros):
+    resultado = []
+    eh_par = lambda x: x % 2 == 0 
+    for n in numeros:
+        if eh_par(n):
+            resultado.append(n)
+    return resultado
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+pares = filtrar_pares(lista)
+print(pares)  

a5ef089c1ace2d658995df0d184dff1f2a00c1c1
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-13 14:32:27 -0300

    update Modulo_02/aula_04_/exercicio_19.py

--- Modulo_02/aula_04_/exercicio_19.py
+++ Modulo_02/aula_04_/exercicio_19.py
@@ -1,3 +1,28 @@
 """19. Ordenador de Dicionários com lambda: Crie uma função que receba uma lista de
 dicionários (cada um com "nome" e "idade"). Use uma lambda e um loop para ordenar a
 lista pela idade e retornar a lista ordenada."""
+
+def ordenar_dicionarios(lista: list[dict]) -> list[dict]:
+    n = len(lista)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if lista[j]["idade"] > lista[j + 1]["idade"]:
+                # Troca os elementos de lugar
+                lista[j], lista[j + 1] = lista[j + 1], lista[j]
+    return lista
+
+
+# Lista de dicionários (desordenada de propósito)
+pessoas = [
+    {"nome": "Beatriz", "idade": 30},
+    {"nome": "Ana", "idade": 25},
+    {"nome": "Carlos", "idade": 19}
+]
+
+print("Antes de ordenar:")
+print(pessoas)
+
+ordenadas = ordenar_dicionarios(pessoas)
+
+print("\nDepois de ordenar:")
+print(ordenadas)

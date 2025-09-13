878a1e66153a6ba16c17fe2ac0f498c9f64b4df3
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-13 14:26:27 -0300

    update Modulo_02/aula_04_/exercicio_14.py

--- Modulo_02/aula_04_/exercicio_14.py
+++ Modulo_02/aula_04_/exercicio_14.py
@@ -1,5 +1,6 @@
 """14. Gerador de Fibonacci: Crie uma função que receba um número inteiro n e retorne uma
 lista com a sequência de Fibonacci até o n-ésimo termo."""
+
 def fibonacci(n: int) -> list[int]:
     sequencia = []
     a, b = 0, 1

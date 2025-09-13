f7cc81ff7f8aeb2c07fad9f4663006eaa676bbfc
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-13 12:51:54 -0300

    update Modulo_02/aula_04_/exercicio_13.py

--- Modulo_02/aula_04_/exercicio_13.py
+++ Modulo_02/aula_04_/exercicio_13.py
@@ -1,2 +1,11 @@
 """13. Validador de Senha: Crie uma função que receba uma senha (string) e retorne True se
 ela tiver pelo menos 8 caracteres e False caso contrário."""
+
+def validador_senha(senha: str) -> bool:
+  if len(senha) >= 8:
+    return True
+  else:
+    return False
+    
+senha = input("Digite sua senha: ")
+print(validador_senha(senha))

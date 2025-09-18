4d62754c418f229cb83a35b481512b64accd1a8a
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-18 18:14:44 -0300

    update Modulo_02/Aula_06/Exercicio_07.py

--- Modulo_02/Aula_06/Exercicio_07.py
+++ Modulo_02/Aula_06/Exercicio_07.py
@@ -3,3 +3,4 @@ Crie uma classe Retangulo que é inicializada com base e altura. Crie dois méto
 1. calcular_area(): deve retornar o cálculo base * altura.
 2. calcular_perimetro(): deve retornar o cálculo 2 * (base + altura).
 Crie um retângulo, chame os dois métodos e imprima os resultados que eles retornam."""
+

cf71c3cf57f40be1e48b1498fa6bbae02975dcbd
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-18 17:24:17 -0300

    update Modulo_02/Aula_06/Exercicio_05.py

--- Modulo_02/Aula_06/Exercicio_05.py
+++ Modulo_02/Aula_06/Exercicio_05.py
@@ -2,3 +2,8 @@
 Crie uma classe ContaBancaria. Toda conta deve começar com um titular e um saldo inicial.
 Crie um método depositar(valor) que some um valor ao saldo da conta. Crie um objeto,
 deposite um valor e imprima o novo saldo."""
+
+class ContaBancaria:
+  def __init__(self, titular: str, saldo: float) -> None:
+    self.titular = nome 
+    self.saldo = saldo

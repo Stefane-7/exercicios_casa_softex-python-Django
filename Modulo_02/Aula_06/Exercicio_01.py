ce2a8d29192bf20cd5590933918e515ceedaf507
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-18 16:33:05 -0300

    update Modulo_02/Aula_06/Exercicio_01.py

--- Modulo_02/Aula_06/Exercicio_01.py
+++ Modulo_02/Aula_06/Exercicio_01.py
@@ -1,3 +1,4 @@
-"""Exercício 1: Molde de uma Pessoa
-Crie uma classe chamada Pessoa. No "registro de nascimento" (__init__), toda pessoa deve
-ter um nome e uma idade."""
+"""   Exercício 1: Molde de uma Pessoa
+
+Crie uma classe chamada Pessoa. 
+No "registro de nascimento" (__init__), toda pessoa deve ter um nome e uma idade."""

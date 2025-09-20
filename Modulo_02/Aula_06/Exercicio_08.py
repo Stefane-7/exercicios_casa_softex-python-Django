7e0684cb1b322d371516676c3c0715dda4c27b34
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-20 11:16:55 -0300

    update Modulo_02/Aula_06/Exercicio_08.py

--- Modulo_02/Aula_06/Exercicio_08.py
+++ Modulo_02/Aula_06/Exercicio_08.py
@@ -4,3 +4,13 @@ Crie uma classe Carro com os atributos modelo e nivel_combustivel (começando co
 2. Crie um método dirigir(distância) que consome combustível (ex: 1 litro a cada 10 km). O
 método deve verificar se há combustível suficiente para a viagem. Se houver, diminua o
 combustível e avise que o carro andou. Se não, avise que não há combustível."""
+
+
+class Carro:
+  def __init__(self, modelo: str, nivel_combustivel: float) -> None:
+    self.modelo = modelo
+    self.nivel_combustivel = nivel_combustivel
+    
+  def abastecer(self, valor: float):
+    if valor > 0:
+      self.nivel_combustivel = valor

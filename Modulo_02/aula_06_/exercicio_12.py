75de0725ddbda09175691465ae5575fb227f8a14
stefane-7 <225245629+Stefane-7@users.noreply.github.com>
2025-09-22 09:00:04 -0300

    ...

--- Modulo_02/aula_06_/exercicio_12.py
+++ Modulo_02/aula_06_/exercicio_12.py
@@ -6,3 +6,18 @@ Futuro' (1985) - Diretor: Robert Zemeckis".
 Depois de implementar, crie um objeto Filme e simplesmente use print(meu_filme).
 ● Dica: O que o método __str__ retorna (return) é o que será exibido quando o objeto for
 impresso."""
+
+
+class Filme:
+    def __init__(self, titulo: str, diretor: str, ano: int):
+        self.titulo = titulo
+        self.diretor = diretor
+        self.ano = ano
+
+    def __str__(self):
+        # "Filme: 'De Volta para o Futuro' (1985) - Diretor: Robert Zemeckis".
+        return f"Filme: '{self.titulo}' ({self.ano}) - Diretor: {self.diretor}"
+
+
+filme1 = Filme("De Volta para o Futuro", "Robert Zemeckis", 1985)
+print(filme1)

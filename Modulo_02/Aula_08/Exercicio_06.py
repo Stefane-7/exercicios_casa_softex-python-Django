ab82c63231f3c5523b90b560965fe35ab0cd58f5
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-26 14:23:35 -0300

    update Modulo_02/Aula_08/Exercicio_06.py

---
+++ Modulo_02/Aula_08/Exercicio_06.py
@@ -0,0 +1,3 @@
+"""6. Encapsulamento e Conjuntos: Crie uma classe RedeSocial com um atributo privado
+__amigos, que deve ser um conjunto para garantir que não haja duplicatas. Implemente
+os métodos adicionar_amigo() e remover_amigo()."""

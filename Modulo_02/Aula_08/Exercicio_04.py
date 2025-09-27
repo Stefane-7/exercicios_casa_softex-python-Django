ecc6414a64e729be52dff2041f402633e8ac022e
Stefane-7 <stefaneoliveira7177@gmail.com>
2025-09-27 12:42:48 -0300

    update Modulo_02/Aula_08/Exercicio_04.py

--- Modulo_02/Aula_08/Exercicio_04.py
+++ Modulo_02/Aula_08/Exercicio_04.py
@@ -3,3 +3,40 @@ de controle de estoque. Comece com uma classe Produto com atributos como nome e
 preco. Em seguida, crie classes filhas como Eletronico e Roupa. A classe Estoque deve
 usar um dicionário para armazenar os produtos, onde a chave é o nome do produto e o
 valor é o objeto do produto."""
+
+
+class Produto:
+  def __init__(self, nome, preco):
+    self.nome = nome
+    self.preco = preco
+
+  def __str__(self):
+    return f"{self.nome} - R${self.preco:.2f}"
+
+class Eletronico(Produto):
+  def __init__(self, nome, preco, quantidade):
+    super().__init__(nome, preco)
+    self.quantidade = quantidade
+
+class Roupa(Produto):
+  def __init__(self, nome, preco, quantidade):
+    super().__init__(nome, preco)
+    self.quantidade = quantidade
+
+class Estoque:
+  def __init__(self):
+    self.estoque = {}
+
+  def adicionar_produto(self, produto):
+    self.estoque[produto.nome] = produto
+
+  def remover_produto(self, nome):
+    if nome in self.estoque:
+      del self.estoque[nome]
+
+  def buscar_produto(self, nome):
+    return self.estoque.get(nome)
+
+  def listar_produtos(self):
+    for produto in self.estoque.values():
+      print(f"{produto.nome} - R${produto.preco} - Quantidade: {produto.quantidade}")
